3dab7172edd08f001adcfcc45cc8dd9a7a0cb41d
Uraz Kağan GÜNEŞ <urazkagangunes@gmail.com>
2024-12-23 17:08:07 +0300

    Add project files.

---
+++ src/core/Core.Persistence/Extensions/Paginate.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Persistence.Extensions;
+
+public class Paginate<T>
+{
+    public Paginate()
+    {
+        Items = Array.Empty<T>();
+    }
+
+    public int Size { get; set; }
+    public int Index { get; set; }
+    public int Count { get; set; }
+    public int Pages { get; set; }
+    public IList<T> Items { get; set; }
+    public bool HasPrevious { get; set; }
+    public bool HasNext => Index + 1 < Pages;
+}
---
+++ src/projects/ECommerce.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace ECommerce.Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationServiceDependencies(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(con => con.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        return services;
+    }
+}
