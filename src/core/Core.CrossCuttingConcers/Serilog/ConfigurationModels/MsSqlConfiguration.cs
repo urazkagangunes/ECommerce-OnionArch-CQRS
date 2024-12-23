3e5ddc07e97c8abda82f7ef099dc0fed36b63ef8
Uraz Kağan GÜNEŞ <urazkagangunes@gmail.com>
2024-12-23 20:47:11 +0300

    serilog created

---
+++ src/core/Core.CrossCuttingConcers/Serilog/ConfigurationModels/MsSqlConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace Core.CrossCuttingConcers.Serilog.ConfigurationModels;
+
+public class MsSqlConfiguration
+{
+    public string ConnectionString { get; set; } = default!;
+    public string TableName { get; set; } = default!;
+    public bool AutoCreateSqlTable { get; set; }
+
+    public MsSqlConfiguration()
+    {
+
+    }
+
+    public MsSqlConfiguration(string connectionString, string tableName, bool autoCreateSqlTable)
+    {
+        ConnectionString = connectionString;
+        TableName = tableName;
+        AutoCreateSqlTable = autoCreateSqlTable;
+    }
+}
