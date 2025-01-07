0ed36064128cbcb2c2ea74ba2961a20c6f4c3260
Uraz Kağan GÜNEŞ <urazkagangunes@gmail.com>
2025-01-07 23:45:03 +0300

    some bugs fixed

--- src/projects/ECommerce.WebAPI/Controllers/BaseController.cs
+++ src/projects/ECommerce.WebAPI/Controllers/BaseController.cs
@@ -1,15 +1,9 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerce.WebAPI.Controllers
 {
     public class BaseController : ControllerBase
     {
-        //private readonly IMediator mediator;
 
-        //public BaseController(IMediator mediator)
-        //{
-        //    this.mediator = mediator;
-        //}
     }
 }
